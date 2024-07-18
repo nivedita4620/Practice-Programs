caacaead1e4c31a08aa4d02435e7ac2a89540ec4
nivedita4620 <137677576+nivedita4620@users.noreply.github.com>
2024-07-18 11:59:13 +0530

    Add files via upload

---
+++ MyFirstMavenProject/src/main/java/com/anudip/demo/App.java
@@ -0,0 +1,13 @@
+package com.anudip.demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
